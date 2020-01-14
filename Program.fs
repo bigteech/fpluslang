ab28841fc62913815f59928649b2af0d80f84bd0
weipeng.wang <weipeng.wang@gllue.com>
2020-01-14 17:56:40 +0800

    :star2: [#59819]

--- Program.fs
+++ Program.fs
@@ -17,83 +17,84 @@ type Token =
     | MulToken
     | DiviToken
 
-let isWordChar a =
-    (a > 'a' && a < 'z') || (a > 'A' && a < 'Z') || a = '_' || a = '-'
+module Lexical = 
+    let isWordChar a =
+        (a > 'a' && a < 'z') || (a > 'A' && a < 'Z') || a = '_' || a = '-'
 
-let rec parseWord (text:string) index =
-    if isWordChar text.[index] then
-        parseWord text (index + 1)
-    else
-        index
+    let rec getWord (text:string) index =
+        if isWordChar text.[index] then
+            getWord text (index + 1)
+        else
+            index
 
-let rec parseString (text: string ) index =
-    if text.[index] = '"' then
-        index
-    else
-        parseString text (index + 1)
+    let rec getString (text: string ) index =
+        if text.[index] = '"' then
+            index
+        else
+            getString text (index + 1)
 
-let parseKeyWord text=
-    match text with
-      | "if" ->
-          IfToken, 2
-      | "let" ->
-          BindToken, 3
-      | _ ->
-          VarToken(text), text.Length
+    let getKeyWord text=
+        match text with
+          | "if" ->
+              IfToken, 2
+          | "let" ->
+              BindToken, 3
+          | _ ->
+              VarToken(text), text.Length
 
-let rec getToken (text:string) (index: int) (indexHandler: int -> unit) =
-    match text.[index] with
-        | a when (a > 'a' && a < 'z') || (a > 'A' && a < 'Z') ->
-           parseWord text index
-            |> (fun x ->
-                indexHandler x;
-                StringToken text.[index..x]
-                )
-            |> (fun x ->
-                parseKeyWord x
-                |> (fun (x,y) ->
-                    indexHandler y
-                    x
+    let rec getToken (text:string) (index: int) (indexHandler: int -> unit) =
+        match text.[index] with
+            | a when (a > 'a' && a < 'z') || (a > 'A' && a < 'Z') ->
+              getWord text index
+                |> (fun x ->
+                    indexHandler x;
+                    StringToken text.[index..x]
                     )
-                )
-        | '"' ->
-            parseString text index
-            |> (fun x ->
-                indexHandler x;
-                StringToken text.[index..x]
-                )
-        | '{' ->
-           indexHandler (index+1)
-           BlockToken
-        | '(' ->
-           indexHandler (index+1)
-           ConditionToken
-        | '.' ->
-           indexHandler (index + 1)
-           DotToken
-        | '+' ->
-            indexHandler (index+1)
-            PlusToken
-        | '-' ->
-            indexHandler (index+1)
-            MinToken
-        | '/' ->
-            indexHandler (index+1)
-            DiviToken
-        | '*' ->
-            indexHandler (index+1)
-            MulToken
-        | ' ' ->
-            getToken text (index+1) indexHandler
-        | '\n' ->
-            getToken text (index+1) indexHandler
+                |> (fun x ->
+                    getKeyWord x
+                    |> (fun (x,y) ->
+                        indexHandler y
+                        x
+                        )
+                    )
+            | '"' ->
+                getString text index
+                |> (fun x ->
+                    indexHandler x;
+                    StringToken text.[index..x]
+                    )
+            | '{' ->
+              indexHandler (index+1)
+              BlockToken
+            | '(' ->
+              indexHandler (index+1)
+              ConditionToken
+            | '.' ->
+              indexHandler (index + 1)
+              DotToken
+            | '+' ->
+                indexHandler (index+1)
+                PlusToken
+            | '-' ->
+                indexHandler (index+1)
+                MinToken
+            | '/' ->
+                indexHandler (index+1)
+                DiviToken
+            | '*' ->
+                indexHandler (index+1)
+                MulToken
+            | ' ' ->
+                getToken text (index+1) indexHandler
+            | '\n' ->
+                getToken text (index+1) indexHandler
 
 let parseStatement (text:string) (index: int) =
-    let token = getToken text index ignore
+    let token = Lexical.getToken text index ignore
     match token with
         | VarToken x ->
             let mutable indexTemp = index + 1
-            let nextToken = getToken text indexTemp (fun x -> indexTemp <- x)
+            let nextToken = Lexical.getToken text indexTemp (fun x -> indexTemp <- x)
             match nextToken with
                 | DotToken ->
                     ()
