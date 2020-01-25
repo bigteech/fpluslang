9ef18d5da322a9c87a9e7560f071b550e66c6abd
wangweipeng <weipeng.wang@gllue.com>
2020-01-26 01:57:50 +0800

    add

--- Program.fs
+++ Program.fs
@@ -1,7 +1,6 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
-
 type Token =
     | StringToken of string
     | NumberToken of int
@@ -10,7 +9,7 @@ type Token =
     | Statement
     | IfToken
     | BindToken
-    | VarToken of string
+    | IdenToken of string
     | DotToken
     | AddToken
     | SubToken
@@ -43,6 +42,28 @@ type Op =
     | IfElseConditionEnd | Else
     | IfEnd
 
+let getLevelByToken token =
+    match token with 
+        | AddToken -> 2
+        | MulToken -> 1
+
+
+type ParseState = {
+    mutable index: int32
+    text: string
+    mutable op: Op list
+    mutable token: Token list
+}
+
+type ParseState with 
+    member z.getLastToken()=z.token.[z.token.Length - 1]
+    member z.moveNextDefault()=z.index <- z.index + 1
+    member z.moveNext x =z.index <- z.index + x
+    member z.pushOp x = z.op <- z.op @ [x] 
+    member z.pushToken x = z.token <-  z.token @ [x]
+
+
+
 module Lexical = 
     let isWordChar a =
         (a > 'a' && a < 'z') || (a > 'A' && a < 'Z') || a = '_' || a = '-'
@@ -50,6 +71,11 @@ module Lexical =
     let rec getWord (text:string) index =
         if isWordChar text.[index] then
             getWord text (index + 1) else
+            index
+
+    let rec getNumber (text: string ) index =
+        if Char.IsNumber text.[index] then
+            getNumber text (index+1) else
             index
 
     let rec getString (text: string ) index =
@@ -65,101 +91,167 @@ module Lexical =
           | "let" ->
               BindToken, 3
           | _ ->
-              VarToken(text), text.Length
+              IdenToken(text), text.Length
 
-    let rec getToken (text:string) (index: int) =
+    let opLevelMap = {| Add = 2; Mul = 1 |}
+
+    let rec nextToken (parseState) : Token=
+        let text = parseState.text
+        let index = parseState.index
+
+        let moveNext x = 
+            parseState.moveNext x
+
+        let moveNextDefault () = 
+            parseState.moveNextDefault()
+
         match text.[index] with
-            | a when (a > 'a' && a < 'z') || (a > 'A' && a < 'Z') ->
-              getWord text index
-                |> (fun x ->
-                    getKeyWord text.[index..x]
-                    )
-                |> (fun (x,y) ->
-                    x, y+1
-                    )
-            | '"' ->
-                getString text index
-                |> (fun x ->
-                    StringToken text.[index..x], x + 1
-                    )
-            | '{' ->
-              BlockToken, index + 1
-            | '(' ->
-                LeftParentheses,index+1
-            | '.' ->
-              DotToken, index+1
-            | '+' ->
-                AddToken, index+1
-            | '-' ->
-                SubToken,index+1
-            | '/' ->
-                DiviToken, index+1
-            | '*' ->
-                MulToken,index+1
-            | ' ' ->
-                getToken text (index+1)
-            | '\n' ->
-                NewLineToken,index+1
-            | ';' ->
-                SemiToken,index+1
+              | a when (a > 'a' && a < 'z') || (a > 'A' && a < 'Z') ->
+                getWord text index
+                  |> (fun x ->
+                      getKeyWord text.[index..x]
+                      )
+                  |> (fun (x,y) ->
+                      moveNext (y-index)
+                      parseState.pushToken x
+                      )
+              | a when Char.IsNumber a-> 
+                  getNumber text index
+                  |> (fun x ->
+                      moveNext (x-index)
+                      let t = text.[index..x-1]
+                      parseState.pushToken (NumberToken (Int32.Parse t))
+                      )
+              | '"' ->
+                  getString text index
+                  |> (fun x ->
+                      moveNext(x)
+                      parseState.pushToken (StringToken text.[index..x])
+                      )
+              | '{' ->
+                  moveNextDefault()
+                  parseState.pushToken BlockToken
 
+              | '(' ->
+                  moveNextDefault()
+                  parseState.pushToken LeftParentheses
+              | '.' ->
+                  moveNextDefault()
+                  parseState.pushToken DotToken
+              | '+' ->
+                  moveNextDefault()
+                  parseState.pushToken AddToken
+              | '-' ->
+                  moveNextDefault()
+                  parseState.pushToken SubToken
+              | '/' ->
+                  moveNextDefault()
+                  parseState.pushToken DiviToken
+
+              | '*' ->
+                  moveNextDefault()
+                  parseState.pushToken MulToken
+
+              | ' ' ->
+                  moveNextDefault() 
+                  nextToken parseState |> ignore
+              | '\n' ->
+                  moveNextDefault()
+                  parseState.pushToken NewLineToken
+              | ';' ->
+                  moveNextDefault()
+                  parseState.pushToken SemiToken
+
+        parseState.getLastToken()
+    
 module Grammer =
     type Node = {
         tokenType: Token
         children: Node list
     }
 
+let getCharByToken token = 
+    match token with
+        |  NumberToken x -> x.ToString()
+        
+
+let getOpByToken token = 
+    match token with
+        |  MulToken  -> Mul
+        | AddToken -> Add
+        
+
 module rec Parser =
-    let parseIfExperience (text:string) (index:int) =
-        // Parser.parseExperience text index
-        IfStart :: [IfEnd]
-
-    let parseTunple text index =
-        []
+    let parseUnary parseState =
+        ()
 
 
-    let parseGet text index=
-        let nextToken, indexTemp = Lexical.getToken text index
-        let nextToken2, indexTemp2 = Lexical.getToken text indexTemp
-        match nextToken2 with
-            | VarToken x->
-                [Get x], indexTemp2
-            | _ -> [Throw], indexTemp2
+    let parseExperienceBinary parseState  =
+        let nextToken () = Lexical.nextToken parseState
+        parseState.pushOp (LoadConst (getCharByToken (nextToken()) ))
+
+        let rec temp token =
+            match token with
+                | MulToken | AddToken | DiviToken | SubToken->
+                    let tokenNext1 = nextToken()
+                    let tokenNext = nextToken()
+                    if tokenNext = SemiToken then
+                        parseState.pushOp (LoadConst (getCharByToken tokenNext1))
+                        parseState.pushOp (getOpByToken token)
+                        SemiToken
+                    else
+                        let level1 = getLevelByToken token
+                        let level2= getLevelByToken tokenNext
+                        if level2 >= level1 then // 比自己大，算自己
+                            parseState.pushOp (LoadConst (getCharByToken tokenNext1))
+                            parseState.pushOp (getOpByToken token)
+                            temp tokenNext
+                        else // 比自己小，优先级高
+                            parseState.pushOp (LoadConst (getCharByToken tokenNext1))
+                            let nextNextToken = temp tokenNext
+                            parseState.pushOp (getOpByToken token)
+                            match nextNextToken with
+                                | SemiToken -> SemiToken
+                                | _ -> temp nextNextToken
+                            
+
+        temp (nextToken()) |> ignore
+
+    let parseExperience  parseState =
+        parseExperienceBinary parseState
+
+    let parseSourceElement text =
+         let parseState = {text=text;index=0;op=[];token=[]}
+         parseExperience  parseState
+         parseState.op
+
+module Vm = 
+    let evalOplist oplst =
+        let stack = Collections.Stack()
+        let eval op =
+            match op with
+                | LoadConst x ->
+                    stack.Push x
+                    0
+                | Add ->
+                    let l1 = Int32.Parse(stack.Pop().ToString())
+                    let l2 = Int32.Parse(stack.Pop().ToString())
+                    stack.Push (l1 + l2)
+                    0
+                | Mul ->
+                    let l1 = Int32.Parse(stack.Pop().ToString())
+                    let l2 = Int32.Parse(stack.Pop().ToString())
+                    stack.Push (l1 * l2)
+                    0
+        for x in oplst do
+            eval x |> ignore
+        printf "%s" (stack.Pop().ToString())
 
 
-    let parseParams text index =
-        let nextToken, indexTemp = Lexical.getToken text index
-        match nextToken with 
-            | AddToken ->
-                [Add]
-            | _ -> 
-                parseExperience text indexTemp
-
-
-    let parseExperience (text:string) (index: int) =
-        let nextToken,indexTemp = Lexical.getToken text index
-        match nextToken with
-            | VarToken x ->
-                let nextToken2,indexTemp2 = Lexical.getToken text indexTemp
-                match nextToken2 with
-                    | DotToken ->
-                        parseGet text indexTemp2
-                |> (fun (x,y) ->
-                    x @ parseParams text y
-                )
-            | AddToken ->
-                [Add]
-            | SubToken ->
-                [Sub]
-            | MulToken ->
-                [Mul]
-            | DiviToken ->
-                [Divi]
-            | IfToken  ->
-                parseIfExperience text indexTemp
-            | SemiToken ->
-                [EndExp]
 [<EntryPoint>]
 let main argv =
-    0
+   Parser.parseSourceElement "3 + 14 + 2 + 6 * 1 + 1;"
+   |> Vm.evalOplist
+   |> ignore
+   0
 
